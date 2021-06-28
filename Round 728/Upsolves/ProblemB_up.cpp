a07483dc8a4344b72baaabc54285f0808773ebb5
Sauman Das <saumand9424@gmail.com>
2021-06-28 11:44:33 -0400

    upsolved problem B

--- Round 728/Upsolves/ProblemB_up.cpp
+++ Round 728/Upsolves/ProblemB_up.cpp
@@ -16,8 +16,23 @@ int main() {
     int t; cin >> t;
     while (t--) {
         int n; cin >> n;
-
-
+        map<int, int> index;
+        for (int i = 1; i <= n; i++) {
+            int k; cin >> k;
+            index.insert(mp(k, i));
+        }
+        int total = 0;
+        for (ll sum = 3; sum <= 2*n-1; sum++) { // sums
+            for (ll i = 1; i*i <= sum; i++) { // factors
+                if (sum%i == 0) {
+                    ll f2 = sum/i;
+                    if (index.find(i) != index.end() && index.find(f2) != index.end() && index[f2] + index[i] == sum && index[f2] != index[i]) {
+                        total++;
+                    }
+                }
+            }
+        }
+        cout << total << endl;
 
 
     }
